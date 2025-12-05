99989766c6285c9d1465eb22e1c058a07392a2c6
AANSH GARG <168648571+ANSH2736@users.noreply.github.com>
2025-12-05 08:12:01 +0530

    Update import statement for SystemMessage and HumanMessage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain.schema import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage
+
 
 
 # Set up Streamlit UI
